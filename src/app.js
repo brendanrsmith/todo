1b3037c1d0f5cb9d30527b02a9a6d1b6b5a3bedd
Brendan Smith <brendan5320@gmail.com>
2021-06-01 22:36:25 -0500

    compile bug fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import ToDo from './components/todo/todo.js';
