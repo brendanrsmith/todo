b3cde5e8dc34c5dfa0f3a27037fcdf6ffe3a2b39
Brendan Smith <brendan5320@gmail.com>
2021-06-05 13:43:05 -0500

    refactor ajax calls

---
+++ src/components/hooks/todo-api-hook.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import useAjax from './ajax-hook'
+const BASE_URL = 'https://brsmith-api-server.herokuapp.com/todos';
+
+const useTodoApi = () => {
+  const [isLoading, list, error, request] = useAjax();
+  const [isUpdating, updateResponse, updateError, updateRequest] = useAjax();
+
+  
+  const getTodoItems = () => {
+    request(BASE_URL, 'GET')
+  }
+  
+  const addItem = (item) => {
+    updateRequest(BASE_URL, 'POST', item);
+  }
+  
+  const deleteItem = (id) => {
+    const url = `${BASE_URL}/${id}`;
+    updateRequest(url, 'DELETE');
+  }
+  
+  const toggleComplete = (item) => {
+    item.complete = !item.complete;
+    const id = item._id;
+    const url = `${BASE_URL}/${id}`;
+    updateRequest(url, 'PUT', item);
+  }
+  
+  useEffect(getTodoItems, [updateResponse, updateError]);
+
+  return [isLoading, list === null ? [] : list, getTodoItems, addItem, deleteItem, toggleComplete];
+}
+
+export default useTodoApi;
