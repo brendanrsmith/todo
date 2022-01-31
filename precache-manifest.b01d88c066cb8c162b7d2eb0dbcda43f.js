8a1c232ad0a107f6b503d716a881555c6e8b4bac
Brendan Smith <brendan5320@gmail.com>
2022-01-31 11:43:59 -0800

    Updates

---
+++ precache-manifest.b01d88c066cb8c162b7d2eb0dbcda43f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9ae11d3dffa68ce5ec3f71e4442f85",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "82e2f3d93d171588b724",
+    "url": "/todo/static/css/2.54c80412.chunk.css"
+  },
+  {
+    "revision": "4964ff9ea76615c04c60",
+    "url": "/todo/static/css/main.7e261ad8.chunk.css"
+  },
+  {
+    "revision": "82e2f3d93d171588b724",
+    "url": "/todo/static/js/2.88d19988.chunk.js"
+  },
+  {
+    "revision": "c1bdf306936ddd82567a345834b1f9d5",
+    "url": "/todo/static/js/2.88d19988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4964ff9ea76615c04c60",
+    "url": "/todo/static/js/main.45f6bbd5.chunk.js"
+  },
+  {
+    "revision": "4b7c96a078d3292937b1",
+    "url": "/todo/static/js/runtime-main.d4ce9cd5.js"
+  }
+]);
